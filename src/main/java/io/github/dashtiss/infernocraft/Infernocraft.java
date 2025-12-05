9b4135c21961b606b3b6cc01ac572a6a020a5d87
Dashtiss <brandontisserand@outlook.com>
2025-12-04 18:51:06 -0600

    First commit

---
+++ src/main/java/io/github/dashtiss/infernocraft/Infernocraft.java
@@ -0,0 +1,10 @@
+package io.github.dashtiss.infernocraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Infernocraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/dashtiss/infernocraft/client/InfernocraftClient.java
@@ -0,0 +1,10 @@
+package io.github.dashtiss.infernocraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfernocraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
